b4a4437fd33ee38b3cb114d813247e767f35ff7d
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-12-11 22:35:14 +0100

    Update views.py

--- store/views.py
+++ store/views.py
@@ -11,7 +11,7 @@ from .forms import MyUserForm
 from django.contrib.auth.forms import UserCreationForm
 
 def store(request):
-	data = cartData(request)
+	data = cartData(request) 
 	
 	cartItems = data['cartItems']
 	order = data['order']
